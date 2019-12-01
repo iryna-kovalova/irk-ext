a34a2a3194279bd37e44a11566af2031cbf0d12a
yury_kovalev <yury_kovalev@ukr.net>
2019-12-01 21:09:44 +0200

    added academic library

---
+++ a.js
@@ -0,0 +1,6 @@
+if(window.location.host == "translate.academic.ru"){
+	document.getElementById('search-query').value = '';
+}
+if(window,location.host == 'la-conjugaison.nouvelobs.com'){
+	document.getElementById('v').focus();
+}
